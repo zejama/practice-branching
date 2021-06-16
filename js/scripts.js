c717d253e21dc4d01fdff2f2292a06a050737fa8
Zeinab Jama <zejama99@gmail.com>
2021-06-15 21:10:50 -0700

    fix jquery error

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
+
 $(document).ready(function() {
-  const over18 = confirm("Are you over the age of 18? Click ok for yes or cancel for no.");
+  confirm("Are you over the age of 18? Click ok for yes or cancel for no.");
 
   if (over18) {
     $("#voting").show();
